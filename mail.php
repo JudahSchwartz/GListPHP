682f04f1207bafa71b2b34679a2305ae12d5228d
JudahSchwartz <judahsch@gmail.com>
2017-11-26 16:17:32 -0500

    Trying to add mailer

---
+++ mail.php
@@ -0,0 +1,27 @@
+<?php
+require 'PHPMailer-master/PHPMailerAutoload.php';
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->SMTPSecure = 'ssl';
+$mail->SMTPAuth = true;
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 465;
+$mail->Username = 'judahschspam@gmail.com';
+$mail->Password = 'hoopdyloopdy';
+$mail->setFrom('judahschspam@gmail.com');
+$mail->addAddress('judahschspam@gmail.com');
+$mail->Subject = 'Hello from PHPMailer!';
+$mail->Body = 'This is a test.';
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "ERROR: " . $mail->ErrorInfo;
+} else {
+    echo "SUCCESS";
+}
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
