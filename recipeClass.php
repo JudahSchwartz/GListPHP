0fdfd6b3e68385636b7c7f26e453477bbf2e6aba
JudahSchwartz <judahsch@gmail.com>
2017-12-05 18:34:15 -0500

    Adding the recipe class

--- recipeClass.php
+++ recipeClass.php
@@ -1,2 +1,40 @@
 <?php
-
+ require "accessDB.php";
+ class Recipe{
+   public $name;
+   public $cookTime;
+   public $DPMStatus;
+   public $instructions = array();
+   public $recipeID = -1;
+   public $userIDSpecific = -1;
+    public function __construct() {
+        
+    }
+    public function create($nam,$ct,$dpm,$inst,$uid)
+    {
+        $name = $name;
+        $cookTime = $ct;
+        $DPMStatus = $dpm; $instructions = $inst;
+        $userIDSpecific = $uid;
+    }
+    public function fromDB($id){
+        require 'accessDB.php';
+        $result = $db->query("SELECT * FROM recipes WHERE RecipeID = '$id'");
+        $obj = $result->fetch_object();
+        $name = $obj->RecipeName;
+        $cookTime = $obj->CookTime;
+        $DPMStatus = $obj-> DPMStatus;
+        $instructions = $obj->Instructions;
+        $recipeID = $obl->$RecipeID;      
+        
+        $db->query();
+    }
+    public function addToDB(){
+        
+    }
+}
+class ingredient{
+    
+    public $name;
+    public $quantity;
+}
